ee8fda432a7e0116f43e64663b2759b0d5b80448
Lucas Boyd <Lucas@new-host.home>
2013-12-10 21:21:31 -0500

    block moving correctly

--- js/canvas.js
+++ js/canvas.js
@@ -27,8 +27,10 @@ function trackMouse(event){
 }
 
 function calculateMovement(mousePos){
-	var dx = mousePos[0] - x;
-	var dy = mousePos[1] - y;
+	var squareCenterX = x + width/2;
+	var squareCenterY = y + height/2;
+	// var dx = mousePos[0] - x;
+	// var dy = mousePos[1] - y;
 // 	var radians = Math.atan2(dy, dx)
 // 	//var direction = radians / Math.PI * 180;
 // 	var ang = radians * 180 / Math.PI;
@@ -44,29 +46,34 @@ function calculateMovement(mousePos){
 	// notes, the square is being gaged bu the upper left corner
 	// I think the p3x p2x is working
 	// using those I need to add or subtract to where x and y go...
-	var d = 10;
-	var mag = Math.sqrt(Math.pow(mousePos[0] - x, 2) + Math.pow(mousePos[1] - y, 2));
-	console.log('mouse :  ' + mousePos);
-	console.log('square: ' + x + ' y: ' + y);
-	var P3x = mousePos[0] + d * (mousePos[0] - x) / mag;
-	var P3y = mousePos[1] + d * (mousePos[1] - y) / mag;
-//	var y2 = dy/dx;
+
+	var d = 1;
+	var mag = Math.sqrt(Math.pow(mousePos[0] - squareCenterX, 2)
+			 + Math.pow(mousePos[1] - squareCenterY, 2));
+// 	console.log('mouse :  ' + mousePos);
+// 	console.log('square: ' + x + ' y: ' + y);
+	var P3x = d * (mousePos[0] - squareCenterX) / mag;
+	var P3y = d * (mousePos[1] - squareCenterY) / mag;
+// //	var y2 = dy/dx;
 	console.log('new ' + P3x + ', ' + P3y);
+// 	return [P3x, P3y];
+	// function alterNum(num){
+	// 	return (num/10).toFixed();
+	// }
 	return [P3x, P3y];
 }
 
 function move(event){
 	var mousePos = trackMouse(event);
-	calculateMovement(mousePos);
-	var y2 = 1;
+	var direction = calculateMovement(mousePos);
 	var timer = setInterval(function(){
-	x = x + 1;
-	y = y + y2;
+	x = x - direction[0];
+	y = y - direction[1];
 	i++;
 	// console.log("x =" + x + "y =" + y);
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	context.fillRect(x, y, width, height);
-	if(i > 50){
+	if(i > 100){
 		clearInterval(timer);
 		i = 0;
 	}
