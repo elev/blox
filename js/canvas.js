04a5a0686d586bf8a796e1dac652f5ecf5318407
Lucas Boyd <Lucas@new-host.home>
2013-12-10 21:49:05 -0500

    fixed set time out issues

--- js/canvas.js
+++ js/canvas.js
@@ -6,6 +6,7 @@ var y = 50;
 var width = 50;
 var height = 50;
 var i = 0;
+var timer;
 context.fillRect(x, y, width, height);
 
 function trackMouse(event){
@@ -29,54 +30,32 @@ function trackMouse(event){
 function calculateMovement(mousePos){
 	var squareCenterX = x + width/2;
 	var squareCenterY = y + height/2;
-	// var dx = mousePos[0] - x;
-	// var dy = mousePos[1] - y;
-// 	var radians = Math.atan2(dy, dx)
-// 	//var direction = radians / Math.PI * 180;
-// 	var ang = radians * 180 / Math.PI;
-// //	alert(radians);
-// 	// var vX = Math.cos(radians);
-// 	// var vY = Math.sin(radians);
-// 	alert(ang);
-// 	var vX = Math.cos(ang);
-// 	var vY = Math.sin(ang);
-// 	// alert("x: " + vX + ' y: ' + vY );
-	//var x2 = 1;
-
-	// notes, the square is being gaged bu the upper left corner
-	// I think the p3x p2x is working
-	// using those I need to add or subtract to where x and y go...
-
 	var d = 1;
 	var mag = Math.sqrt(Math.pow(mousePos[0] - squareCenterX, 2)
 			 + Math.pow(mousePos[1] - squareCenterY, 2));
-// 	console.log('mouse :  ' + mousePos);
-// 	console.log('square: ' + x + ' y: ' + y);
+
 	var P3x = d * (mousePos[0] - squareCenterX) / mag;
 	var P3y = d * (mousePos[1] - squareCenterY) / mag;
-// //	var y2 = dy/dx;
-	console.log('new ' + P3x + ', ' + P3y);
-// 	return [P3x, P3y];
-	// function alterNum(num){
-	// 	return (num/10).toFixed();
-	// }
 	return [P3x, P3y];
 }
 
 function move(event){
 	var mousePos = trackMouse(event);
 	var direction = calculateMovement(mousePos);
-	var timer = setInterval(function(){
-	x = x - direction[0];
-	y = y - direction[1];
-	i++;
-	// console.log("x =" + x + "y =" + y);
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	context.fillRect(x, y, width, height);
-	if(i > 100){
+	if (typeof timer != undefined){
 		clearInterval(timer);
 		i = 0;
 	}
+	timer = setInterval(function(){
+		x = x - direction[0];
+		y = y - direction[1];
+		i++;
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.fillRect(x, y, width, height);
+		if(i > 100){
+			clearInterval(timer);
+			i = 0;
+		}
 	}, 10);
 };
 
